9fe94518d08c23f1c6c59511f0cd8cc885f79631
githinjisamson1 <githinjisamson148@gmail.com>
2024-01-16 12:18:13 +0300

    Configure superheroes db

--- server/app.py
+++ server/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
 
 
 @app.route("/")
